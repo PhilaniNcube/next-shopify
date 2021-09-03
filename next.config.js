a6adee178e4cdcc5b4cc1fb86ba9d9145cdf6eb4
Philani Ncube <ncbphi001@gmail.com>
2021-09-03 20:00:47 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const { withFrameworkConfig } = require('./framework/common/config');
+
+module.exports = withFrameworkConfig({
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['en-UK', 'en-ZA'],
+    defaultLocale: 'en-UK',
+  },
+});
+
+console.log('next.config.js', JSON.stringify(module.exports, null, 2));
